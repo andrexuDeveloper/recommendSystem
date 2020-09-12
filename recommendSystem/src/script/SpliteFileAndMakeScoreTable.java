fbe92eb15c06efec66b98a36b935fd5b83605a8f
xuhongcao <1>
2020-09-12 22:42:35 +0800

    add branch dev

--- recommendSystem/src/script/SpliteFileAndMakeScoreTable.java
+++ recommendSystem/src/script/SpliteFileAndMakeScoreTable.java
@@ -16,16 +16,19 @@ import util.FileTool;
 public class SpliteFileAndMakeScoreTable {
 	
 	public static void main(String[] args) throws Exception {
-		//String inputDir = "data/fresh_comp_offline/";
-		//String outputDir = "data/fresh_comp_offline/sample/";
+		String inputDir = "data/";
+		String outputDir = "data/xuhongcao/sample/";
 		//String inputDir = "data/fresh_comp_offline/sample/";
 		//String outputDir = "data/fresh_comp_offline/sample/out/";
-		String inputDir = args[0];
-		String outputDir = args[1];
-		//String userPath = inputDir + "tianchi_fresh_comp_train_user.csv";
-		String userPath = inputDir + args[2];
-		String outputPath = args[3];
-		//String outputPath = outputDir + "user.csv";
+//		String inputDir = args[0];
+//		String outputDir = args[1];
+		String userPath = inputDir + "tianchi_fresh_comp_train_user.csv";
+//		String userPath = inputDir + args[2];
+//		String outputPath = args[3];
+		String outputPath = outputDir + "user.csv";
+
+
+
 		//FileTool.makeSampleData(userPath, true, outputPath, 10000);
 		//List<Object> itemList = FileTool.readFileOne(itemPath, true, ",", "item");
 		//List<User> userList = FileTool.readFileOne(userPath, false, ",", "user");
@@ -35,8 +38,11 @@ public class SpliteFileAndMakeScoreTable {
 		Map<String, Map<String, List<User>>> userMap = DataProcess.mapByUser(userList,userSet,itemSet);
 		userList.clear();
 		DataProcess.output(userMap, outputDir);
+
+
+
 		/*
-		//Éú³ÉuserToItemµÄ´ò·Ö±í
+		//ï¿½ï¿½ï¿½ï¿½userToItemï¿½Ä´ï¿½Ö±ï¿½
 		Map<String, Map<String, Double>> scoreTable = DataProcess.makeScoreTable(userMap);
 		//DataProcess.output(scoreTable, outputDir + "scoreTable.csv" , userSet, itemSet, ",");
 		userMap.clear();
